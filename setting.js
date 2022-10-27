b235aae32eaf160cd147d8faeb1f081a68e06e77
REDA YT <104506935+Thami2007@users.noreply.github.com>
2022-10-27 20:47:08 +0530

    Create setting.js

---
+++ setting.js
@@ -0,0 +1,153 @@
+//Thanks MY FAMILY AND LORD BUDDHA 
+
+//PLEASE SUBSCRIBE REDA YT
+
+//©REDAYT
+
+const fs = require('fs')
+const chalk = require('chalk')
+
+//Api Website\\
+global.APIs = {
+
+	zenz: 'https://zenzapis.xyz',}
+
+//Api Key\\
+
+global.APIKeys = {
+
+	'https://zenzapis.xyz': 'your key',
+
+}
+
+⛔DONT EDIT⛔
+
+global.autoTyping = false //auto tying in gc (true to on, false to off)
+global.autoreadpmngc = false //auto reading in gc and pm (true to on, false to off)
+global.autoReadGc = false //auto reading in gc (true to on, false to off)
+global.autoRecord = false //auto recording (true to on, false to off)
+global.available = true //auto available (true to on, false to off)
+
+//MENU✏️
+
+global.vidmenu = fs.readFileSync("./REDAYT/theme/Marshmello.mp4") //gif and video menu
+//DOCUMENTS VARIANTS✏️
+
+global.doc1 = 'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+global.doc2 = 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+global.doc3 = 'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+global.doc4 = 'application/zip'
+global.doc5 = 'application/pdf'
+global.doc6 = 'application/vnd.android.package-archive'
+
+//OWNER V CARD✏️
+
+global.vcardowner = ['94769285793'] //ur owner number
+global.ownername = "REDA YT" //ur owner name
+global.ytname = "YT: REDA YT" //ur yt chanel name
+global.socialm = "GitHub: Thami2007" //ur github or insta name
+global.location = "Sri lanka , nuwaraeliya" //ur location
+
+//BOT OWNER✏️
+
+global.owner = ['94769285793']
+global.ownertag = '94769285793' //ur tag number
+global.botname = 'Marshmello BOT MD' //ur bot nam
+global.linkz = "https://youtu.be/gFK8154dCbs" //your theme url which will be displayed on whatsapp
+global.websitex = "https://youtube.com/c/REDAFF" //ur website to be displayed
+global.botscript = 'https://github.com/DGXeon/CheemsBot-MD4' //script link
+global.reactmoji = "🥰" //ur menu react emoji
+global.themeemoji = "😈" //ur theme emoji
+global.packname = "Marshmello MD " //ur sticker watermark packname
+global.author = "𝚁𝙴𝙳𝙰 𝚈𝚃" //ur sticker watermark author
+global.aliveimg ="https://telegra.ph/file/38c052d313ccf14d80550.jpg"
+global.allmenuimg ="https://telegra.ph/file/4f8a6f14430de2a4f4991.jpg"
+global.welcomeimg= "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.gbyeimg = "https://telegra.ph/file/844812c77704231c08a90.jpg"
+global.alivemsg = *HAPPYNESS🥰 OF YOUR LIFE DEPENDS ON THE QUALITY OF YOUR THOUGHT*
+
+//BOT THEME⛔[don't edit]
+
+global.thum = fs.readFileSync("./REDAYT/theme/REDAYT.jpg" //ur thumb pic
+global.log0 = fs.readFileSync("./REDAYT/theme/REDAYT.jpg") //ur logo pic
+global.err4r = fs.readFileSync("./REDAYT/theme/REDAYT.jpg") //ur error pic
+global.thumb = fs.readFileSync("./REDAYT/theme/REDAYT.jpg") //ur thumb pic
+
+//DATABASE⛔[don't edit]
+
+global.premium = ['94769285793] //ur premium numbers
+global.ntilinkytvid = []
+global.ntilinkytch = []
+global.ntilinkig = []
+global.ntilinkfb = []
+global.ntilinktg = []
+global.ntilinktt = []
+global.ntilinktwt = []
+global.ntilinkall = []
+global.nticall = []
+global.ntwame = []
+global.nttoxic = []
+global.ntnsfw = []
+global.ntvirtex = []
+global.rkyt = []
+global.wlcm = []
+global.gcrevoke = []
+global.autorep = []
+global.ntilink = []
+
+//OTHER⛔[don't edit]
+
+global.sessionName = 'session'
+global.antitags = true
+global.prefa = ['','!','.']
+global.sp = '⭔'
+global.mess = {
+    success: '🎲🎀_Done_',
+    admin: '🎲🎀 _This Feature Is Only For Admin!_',
+    botAdmin: '🎲🎀 _Bot Must Be Admin First!_',
+    owner: '🎲🎀 _This Feature Is Only For Owner!_',
+    group: '🎲🎀 _Feature Used Only For Groups!_'   
+    private: '🎲🎀 _Features Used Only For Private Chat!_',
+    bot: '🎲🎀 _This Feature Is Only For Bot_',
+    wait: '🎲🎀 _Processing Please Wait..._',
+    linkm: '🤔❓ _Where is the link?_',
+    error: '🤕🤧 ```Error!!```',
+    endLimit: 'Your Daily Limit Has Expired, The Limit Will Be Reset Every 12 Hours',
+   ban: 'You have been banned by the owner, if you want to be unbanned, chat owner.',
+    nsfw: 'The nsfw feature has not been activated, please contact the admin to activate',
+    banChat: 'The bot was banned in this group, please contact the owner to unban'
+
+}
+
+    global.limitawal = {
+    premium: "Infinity",
+    free: 12,
+    monayawal: 1000
+
+}
+
+   global.rpg = {
+   darahawal: 100,
+   besiawal: 15,
+   goldawal: 10,
+   emeraldawal: 5,
+  umpanawal: 5,
+   potionawal: 1
+
+}
+
+global.thumb = fs.readFileSync('./REDAYT/theme/REDAYT.jpg')
+global.flaming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=sketch-name&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.fluming = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=fluffy-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flarun = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=runner-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+global.flasmurf = 'https://www6.flamingtext.com/net-fu/proxy_form.cgi?&imageoutput=true&script=smurfs-logo&doScale=true&scaleWidth=800&scaleHeight=500&fontsize=100&text='
+
+let file = require.resolve(__filename)
+
+fs.watchFile(file, () => {
+	fs.unwatchFile(file)
+	console.log(chalk.redBright(`Update'${__filename}'`))
+	delete require.cache[file]
+	require(file)
+
+})
